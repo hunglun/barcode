148579cc73365be7d1950ba2ff9f1a201dda4712
hunglun <hunglun.o@gmail.com>
2015-12-21 16:03:47 +0800

    Replace UIImageView with UIView Add back camera as input device to capture session. Start capture session, when camera access is granted. Unfortunately, UIView does not show anything, when capture button is pressed.

--- Barcode/ViewController.swift
+++ Barcode/ViewController.swift
@@ -8,31 +8,80 @@
 
 import UIKit
 import AVFoundation
-class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+class ViewController: UIViewController ,  AVCaptureVideoDataOutputSampleBufferDelegate, UINavigationControllerDelegate{
 
-    @IBOutlet var ImageView: UIImageView!
+
+    @IBOutlet var cameraView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
     }
 
+    @IBOutlet var captureButton: UIButton!
     //TODO: create a toolbar at the bottom.
     
     @IBAction func startImageCapture(sender: AnyObject) {
-        //TODO: get preview working!
-        let captureSession = AVCaptureSession() // <#Get a capture session#>;
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        //var previewLayer = AVCaptureVideoPreviewLayer(layerWithSession:captureSession)
-     //   var aView = UIView() //<#The view in which to present the layer#>;
-        previewLayer.frame = ImageView.bounds; // Assume you want the preview layer to fill the view.
-        ImageView.layer.addSublayer(previewLayer)
 
         
+        
+        let captureSession = AVCaptureSession()
+
+        
+        let avCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+        /////////////////////////////////////////////////
+        // Add camera as input device to capture session
+        /////////////////////////////////////////////////
+        if let input = try? AVCaptureDeviceInput(device: avCaptureDevice) where captureSession.canAddInput(input){
+
+            captureSession.addInput(input)
+
+            print("Video input is \(avCaptureDevice.localizedName)")
+
+        }else{
+
+            print("Unable to get video capture device")
+
+            captureButton.enabled = false
+
+            return // do not continue in this function, since video input is not available.
+
+        }
+        ////////////////////////////////////////////////////
+        // Connect captureSession to UIView via previewLayer
+        ////////////////////////////////////////////////////
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+
+        cameraView = UIView() //<#The view in which to present the layer#>;
+        previewLayer.frame = cameraView.bounds; // Assume you want the preview layer to fill the view.
+        cameraView.layer.addSublayer(previewLayer)
+
+        ////////////////////////////////////////////////////////
+        // If camera access is granted, run the capture session
+        ////////////////////////////////////////////////////////
+        AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: {(granted: Bool)  in
+
+            if (granted){
+                print("camera access is granted")
+                captureSession.startRunning()
+            }
+            else {
+                print("camera access is NOT granted")
+            }
+
+        })
+    
+        //TODO: Processing Frames of Video
+        //https://developer.apple.com/library/ios/documentation/AudioVideo/Conceptual/AVFoundationPG/Articles/04_MediaCapture.html
+        
+    
     }
 
 }
